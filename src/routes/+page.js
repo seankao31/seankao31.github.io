9b524e71930263dc01c3cc456e414ce178506953
Sean Kao <seankao31@gmail.com>
2025-11-06 16:51:05 +0800

    update home page content
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,12 +2,6 @@ import { error } from '@sveltejs/kit';
 
 export const load = async () => {
 	try {
-		const ReadMeFile = await import('../../README.md');
-		const ReadMe = ReadMeFile.default;
-
-		return {
-			ReadMe
-		};
 	} catch (err) {
 		error(500, err);
 	}
